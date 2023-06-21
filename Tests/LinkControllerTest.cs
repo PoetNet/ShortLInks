665196893f3a81d80ff520360211b2b38f2335a6
Roman Marfutin <r.marfutn@sh.dunice.net>
2023-06-21 17:55:15 +0300

    first success test is updated

--- Tests/LinkControllerTest.cs
+++ Tests/LinkControllerTest.cs
@@ -12,41 +12,33 @@ namespace ShortLinks.Tests;
 
 public class LinkControllerTest
 {
-    // private readonly Mock<IHttpContextAccessor> _mockHttpContextAccessor;
-    // // private readonly Mock<HttpRequest> _mockHttpRequest;
-    // // private readonly Mock<HttpContext> _mockHttpContext;
-    // private readonly Mock<ILinkService> _mockLinkService;
-    // public LinkControllerTest(Mock<IHttpContextAccessor> mockHttpContextAccessor,
-    //                         //   Mock<HttpRequest> mockHttpRequest,
-    //                         //   Mock<HttpContext> mockHttpContext,
-    //                           Mock<ILinkService> mockLinkService)
-    // {
-    //     _mockHttpContextAccessor = mockHttpContextAccessor;
-    //     // _mockHttpRequest = mockHttpRequest;
-    //     // _mockHttpContext = mockHttpContext;
-    //     _mockLinkService = mockLinkService;
-    // }
+    private readonly Mock<IHttpContextAccessor> _httpContextAccessorMock;
+    private readonly Mock<HttpRequest> _requestMock;
+    private readonly Mock<HttpContext> _httpContextMock;
+    private readonly Mock<ILinkService> _linkServiceMock;
+    public LinkControllerTest()
+    {
+        _httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+        _requestMock = new Mock<HttpRequest>();
+        _httpContextMock = new Mock<HttpContext>();
+        _linkServiceMock = new Mock<ILinkService>();
+    }
 
     [Fact]
     public async void CreateShortLink_ValidRequest_ReturnsOkResult()
     {
-        var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
-        var linkServiceMock = new Mock<ILinkService>();
-        var httpContextMock = new Mock<HttpContext>();
-        var requestMock = new Mock<HttpRequest>();
-
         var createLinkDto = new LinkDto { LinkPath = "https://dotnet.microsoft.com/en-us/apps/aspnet", PasswordToDel = "123098" };
         var expectedLink = new Link { LinkPath = createLinkDto.LinkPath, ShortLink = "abc123" };
 
-        requestMock.Setup(_ => _.Scheme).Returns("http");
-        requestMock.Setup(_ => _.Host).Returns(new HostString("localhost:5277"));
+        _requestMock.Setup(_ => _.Scheme).Returns("http");
+        _requestMock.Setup(_ => _.Host).Returns(new HostString("localhost:5277"));
 
-        httpContextMock.Setup(_ => _.Request).Returns(requestMock.Object);
-        httpContextAccessorMock.Setup(a => a.HttpContext).Returns(httpContextMock.Object);
+        _httpContextMock.Setup(_ => _.Request).Returns(_requestMock.Object);
+        _httpContextAccessorMock.Setup(a => a.HttpContext).Returns(_httpContextMock.Object);
 
-        linkServiceMock.Setup(serviceInstance => serviceInstance.CreateLink(createLinkDto)).ReturnsAsync(expectedLink);
+        _linkServiceMock.Setup(serviceInstance => serviceInstance.CreateLink(createLinkDto)).ReturnsAsync(expectedLink);
 
-        var controller = new LinkController(httpContextAccessorMock.Object, linkServiceMock.Object);
+        var controller = new LinkController(_httpContextAccessorMock.Object, _linkServiceMock.Object);
 
         var result = await controller.CreateShortLink(createLinkDto);
         Assert.IsType<Ok<ShortLinks.Models.PublicLinkView>>(result);
